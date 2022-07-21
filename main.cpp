845c1a9ad3fa32fc842cba7c93d037bd09d614b0
MrZorg111 <yodji333@mail.ru>
2022-07-21 07:24:49 +0300

    Финальная версия

--- main.cpp
+++ main.cpp
@@ -2,16 +2,21 @@
 #include <fstream>
 #include <string>
 
+
 int main() {
     std::ifstream reader;
     std::string address;
-    std::string index, p, n, g;
+    std::string index(4,0);
     std::cout << "Enter the path to the file using a double backslash: ";
     std::cin >> address;
     reader.open(address, std::ios::binary);
-    while (!reader.eof()) {
-        reader >> index /*>> p >> n >> g;*/;
-        std::cout << index.substr(0)/* << " " << p << " " << n << " " << g << " "*/;
+    if(!reader.eof()) {
+        reader >> index;
     }
-
+    if (index[0] == -119 && index.substr(1, 3) == "PNG") {
+        std::cout << "This is file PNG.";
+    }
+    else {
+        std::cout << "This is file not PNG.";
+    }
 }
